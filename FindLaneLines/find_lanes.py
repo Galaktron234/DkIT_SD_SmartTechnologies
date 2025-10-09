f6dd386228f0f805a34ea15b9ce3635624ca848f
johnloane <john.loane@dkit.ie>
2025-10-09 14:07:44 +0100

    Find lane lines in image and video
    Use cv2 to find lane lines in image and video

--- FindLaneLines/find_lanes.py
+++ FindLaneLines/find_lanes.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def canny(image):
-    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     canny = cv2.Canny(blur, 50, 150)
     return canny
@@ -12,8 +12,7 @@ def canny(image):
 
 def region_of_interest(image):
     height = image.shape[0]
-    width = image.shape[1]
-    polygons = np.array([[(250, height), (520, 400), (650, 520), (width, height)]])
+    polygons = np.array([[(0, 700), (500, 300), (960, 1000)]])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
     masked_image = cv2.bitwise_and(image, mask)
@@ -26,16 +25,74 @@ def display_lines(image, lines):
             x1, y1, x2, y2 = line.reshape(4)
             cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
     return line_image
+
+
+def make_coordinates(image, line_parameters):
+    print(line_parameters)
+    if line_parameters is None:
+        return None
+    slope, intercept = line_parameters
+    if line_parameters is not None and abs(slope) > 0.001:
+        y1 = image.shape[1]
+        y2 = int((y1 * (9/20)))
+        x1 = int((y1 - intercept) / slope)
+        x2 = int((y2 - intercept) / slope)
+        return np.array([x1, y1, x2, y2])
+
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    if lines is None:
+        return None
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
+    
             
 
-image = cv2.imread("cavan_road.jpg")
-lane_image = np.copy(image)
-canny = canny(lane_image)
-cropped_image = region_of_interest(canny)
-lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength = 30, maxLineGap = 4)
-line_image = display_lines(lane_image, lines)
-combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-cv2.imshow("Result", combo_image)
-#plt.imshow(canny)
-#plt.show()
-cv2.waitKey(0)
+# image = cv2.imread("cavan_road.jpg")
+# lane_image = np.copy(image)
+# canny = canny(lane_image)
+# cropped_image = region_of_interest(canny)
+# lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength = 30, maxLineGap = 4)
+# averaged_lines = average_slope_intercept(lane_image, lines)
+# line_image = display_lines(lane_image, averaged_lines)
+# combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+# cv2.imshow("Result", combo_image)
+# #plt.imshow(canny)
+# #plt.show()
+# cv2.waitKey(0)
+
+cap = cv2.VideoCapture("JohnM50.mp4")
+while(cap.isOpened()):
+    _, frame = cap.read()
+    if frame is not None:
+        frame = cv2.resize(frame, (960, 1280))
+        lane_image = np.copy(frame)
+        canny_image = canny(lane_image)
+        cropped_image = region_of_interest(canny_image)
+        lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap = 100)
+        averaged_lines = average_slope_intercept(lane_image, lines)
+        line_image = display_lines(lane_image, averaged_lines)
+        combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+        cv2.imshow("Result", combo_image)
+        #plt.imshow(canny)
+        #plt.show()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+cap.release()
+cv2.destroyAllWindows()
